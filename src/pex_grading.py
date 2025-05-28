446da8ee028b5b2a936525154dbfd8255d74e850
TypingTobii <tobias.henig@t-online.de>
2025-05-28 18:42:10 +0200

    start implementing automatic pex grading

---
+++ src/pex_grading.py
@@ -0,0 +1,16 @@
+
+class Feedback:
+    test_output: str = ""
+    additional_feedback: str = ""
+    points: float = 0.0
+
+    def __init__(self, ???) -> None:
+        ???
+
+
+class Grader:
+    created_docker_image: str | None = None
+
+    def __init__(self, ???) -> None:
+        ???
+
